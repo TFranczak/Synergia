256514a5f201935290a1b8fb2f19d803b8e88b6d
TFranczak <franczaktommy@yahoo.com>
2018-07-05 10:54:48 -0500

    Added option to display tuneshift

--- IOTA_opts.py
+++ IOTA_opts.py
@@ -15,6 +15,7 @@ opts.add("dp_over_p", 0.001, "delta p / p")
 opts.add("macro_particles", 10000, "Particles in PIC sim")
 opts.add("turns", 1000, "Amount of turns around synchrotron")
 opts.add("verbosity", 1, "Verbosity")
+opts.add("showTuneShift", True, "Shows the lazlett tuneshift")
 
 # e lens
 opts.add("elens", True, "Use Elens?")
