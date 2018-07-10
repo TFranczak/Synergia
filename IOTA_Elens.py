bf5338ea10fc1f468729851abe9b9839d17b22ab
TFranczak <franczaktommy@yahoo.com>
2018-07-10 07:27:39 -0500

    Update IOTA_Elens.py

--- IOTA_Elens.py
+++ IOTA_Elens.py
@@ -173,7 +173,7 @@ if __name__ == "__main__":
     
     	elens = synergia.lattice.Lattice_element("elens", "lens")
     	elens.set_double_attribute("l", 0.0)
-    	elens.set_double_attribute("eenergy", beta * opts.emittance) #0.01
+    	elens.set_double_attribute("eenergy", sqrt(beta * opts.emittance)) #0.01
     	elens.set_double_attribute("current", current * elens_length * -1)
     	elens.set_double_attribute("radius", 0.001)
     	elens.set_double_attribute("longrms", opts.longrms)
