1c52e62ab713ab8cec762644e0c5c1ed8b66bbd0
TFranczak <franczaktommy@yahoo.com>
2018-07-12 17:24:54 -0500

    Added option for output_frequency

--- IOTA_Elens.py
+++ IOTA_Elens.py
@@ -231,11 +231,14 @@ if __name__ == "__main__":
         diag  = "d_nolens" + str(opts.turns) + ".h5"
         track = "t_nolens" + str(opts.turns) + ".h5"
 
-    # bunch_simulator.add_per_step(synergia.bunch.Diagnostics_full2(diag))    
-    bunch_simulator.add_per_turn(synergia.bunch.Diagnostics_full2(diag))
-    # bunch_simulator.add_per_step(synergia.bunch.Diagnostics_bulk_track(track, opts.particles_tracked))
-    bunch_simulator.add_per_turn(synergia.bunch.Diagnostics_bulk_track(track, opts.particles_tracked))
-    # bunch_simulator.add_per_turn(synergia.bunch.Diagnostics_particles('particles.h5'), 1)
+    # Generates the diagnostic output files
+    if opts.output_frequency == "Step"
+        bunch_simulator.add_per_step(synergia.bunch.Diagnostics_full2(diag))    
+        bunch_simulator.add_per_step(synergia.bunch.Diagnostics_bulk_track(track, opts.particles_tracked))
+    else:
+        bunch_simulator.add_per_turn(synergia.bunch.Diagnostics_full2(diag))
+        bunch_simulator.add_per_turn(synergia.bunch.Diagnostics_bulk_track(track, opts.particles_tracked))
+        # bunch_simulator.add_per_turn(synergia.bunch.Diagnostics_particles('particles.h5'), 1)
 
     # Run the simulation
     synergia.simulation.Propagator(stepper).propagate(
