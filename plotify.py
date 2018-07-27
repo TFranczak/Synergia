1fdf75e4fffabe6ff2346120b9b860f8b880f7e0
TFranczak <franczaktommy@yahoo.com>
2018-07-27 11:13:16 -0500

    Added second plot for emity

--- plotify.py
+++ plotify.py
@@ -10,12 +10,25 @@ if __name__ == "__main__":
     h5_1 = h5py.File(sys.argv[1])
     h5_2 = h5py.File(sys.argv[2])
 
+
+    # Plot x emittance from both
     plt.xlabel("Distance (m)")
     plt.ylabel("Emittance (m)")
 
-    # Plot x emittance from both
-    line1 = plt.plot(h5_1.get("s"), h5_1.get("emitx"), label='X Emit for ' + sys.argv[1].split('_')[1])
-    line2 = plt.plot(h5_2.get("s"), h5_2.get("emitx"), label='X Emit for ' + sys.argv[2].split('_')[1])
+    plt.plot(h5_1.get("s"), h5_1.get("emitx"), label='X Emit for ' + sys.argv[1].split('_')[1])
+    plt.plot(h5_2.get("s"), h5_2.get("emitx"), label='X Emit for ' + sys.argv[2].split('_')[1])
+    plt.tight_layout()
+    plt.legend()
+    plt.axes().set_ylim(ymin=0)
+    plt.show()
+
+    # Plot y emittance from both
+    plt.xlabel("Distance (m)")
+    plt.ylabel("Emittance (m)")
+
+    plt.plot(h5_1.get("s"), h5_1.get("emity"), label='Y Emit for ' + sys.argv[1].split('_')[1])
+    plt.plot(h5_2.get("s"), h5_2.get("emity"), label='Y Emit for ' + sys.argv[2].split('_')[1])
     plt.tight_layout()
     plt.legend()
     plt.show()
+
