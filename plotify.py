ec3b380a3ac1bfa719abf385698c83a08292db7e
TFranczak <franczaktommy@yahoo.com>
2018-07-27 11:22:06 -0500

    Added titles/legend

--- plotify.py
+++ plotify.py
@@ -10,21 +10,26 @@ if __name__ == "__main__":
     h5_1 = h5py.File(sys.argv[1])
     h5_2 = h5py.File(sys.argv[2])
 
+    # Finds elens amplitude from output file names
+    amp1 = sys.argv[1].split('_')[1]
+    amp2 = sys.argv[2].split('_')[1]
 
-    # Plot x emittance from both
+
+    # Plot x emittance from both -----------------------------------
     plt.xlabel("Distance (m)")
     plt.ylabel("Emittance (m)")
+    plt.title("X Emittance for " + amp1 + " and " + amp2 + " VS Distance")
 
     plt.plot(h5_1.get("s"), h5_1.get("emitx"), label='X Emit for ' + sys.argv[1].split('_')[1])
     plt.plot(h5_2.get("s"), h5_2.get("emitx"), label='X Emit for ' + sys.argv[2].split('_')[1])
     plt.tight_layout()
     plt.legend()
-    plt.axes().set_ylim(ymin=0)
     plt.show()
 
-    # Plot y emittance from both
+    # Plot y emittance from both ------------------------------------
     plt.xlabel("Distance (m)")
     plt.ylabel("Emittance (m)")
+    plt.title("Y Emittance for " + amp1 + " and " + amp2 + " VS Distance")
 
     plt.plot(h5_1.get("s"), h5_1.get("emity"), label='Y Emit for ' + sys.argv[1].split('_')[1])
     plt.plot(h5_2.get("s"), h5_2.get("emity"), label='Y Emit for ' + sys.argv[2].split('_')[1])
