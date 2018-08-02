f4165f70c7541a8cf1fa0ff852df8f6dd7edd51e
TFranczak <@users.noreply.github.com>
2018-08-02 17:45:06 +0000

    Updated opts parameters

--- IOTA_opts.py
+++ IOTA_opts.py
@@ -13,19 +13,19 @@ opts.add("dp_over_p", 0.001, "delta p / p")
 
 # simulation options
 opts.add("macro_particles", 100000, "Particles in PIC sim")
-opts.add("turns", 2000, "Amount of turns around synchrotron")
+opts.add("turns", 1000, "Amount of turns around synchrotron")
 opts.add("verbosity", 1, "Verbosity")
 opts.add("spacecharge", True, "Toggles between no space charge and 2D open hockney")
 opts.add("nonLinearLattice", False, "Toggles between T:chef_propagate and F:chef_map; forces linear elements")
 opts.add("showTuneInfo", True, "Shows tune and tuneshift")
 
 # e lens
-opts.add("elens", False, "Use Elens?")
-opts.add("current", 0.1, "Current of Elens")
+opts.add("elens", True, "Use Elens?")
+opts.add("current", 0.5, "Current of Elens")
 opts.add("length", 0.7, "Physical length of Elens")
 
 # output options
-opts.add("particles_tracked", 0, "Number of particle for track diagnostic tool")
-opts.add("output_frequency", "Mark", "Frequency of output; 'Turn', 'Step', or 'Mark'")
+opts.add("particles_tracked", 16, "Number of particle for track diagnostic tool")
+opts.add("output_frequency", "Turn", "Frequency of output; 'Turn', 'Step', or 'Mark'")
 opts.add("checkpoint_period", 0, "Number of turns between checkpoints. 0 for no checkpoints")
-opts.add("output_flags", "Mark", "Append a custom flag to output name. Empty string for no flag")
+opts.add("output_flags", "Turn", "Append a custom flag to output name. Empty string for no flag")
